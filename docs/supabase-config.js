de7419040407b54b47f26bf9c5852f0dff75f6ee
Souhail Ouabi <souhail.ouabi@comim.ma>
2026-07-14 21:43:13 +0200

    Configure Supabase credentials for shared qualifications

--- docs/supabase-config.js
+++ docs/supabase-config.js
@@ -1,4 +1,4 @@
 window.__RADAR_SUPABASE__ = window.__RADAR_SUPABASE__ || {
-  url: "",
-  anonKey: "",
+  url: "https://ydhtyzpnsqwoixaxdssw.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkaHR5enBuc3F3b2l4YXhkc3N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwNDk4OTAsImV4cCI6MjA5OTYyNTg5MH0.K8aXzEjyl_2ae6EwJXrsJcrrSKl3CFkYjjXidI-dFb8",
 };
